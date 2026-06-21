9da512fd0e2aa894049fb3862bc5483263ac142c
cosc2436 <mafloresrr@gmail.com>
2026-06-21 03:45:34 +0000

    initialized stack class. created basic constructor. created class methods push, pop. peek, and isEmpty

---
+++ stack.cpp
@@ -0,0 +1,38 @@
+#include "stack.h"
+#include <iostream>
+
+Stack::Stack() : top(-1), capacity(::DEFAULT_SIZE) {
+    data = new int[capacity];
+}
+
+Stack::~Stack() {
+    delete[] data;
+}
+
+void Stack::push(int value) {
+    if (top + 1 < capacity) {
+        data[++top] = value;
+    } else {
+        std::cerr << "Stack overflow!" << std::endl;
+    }
+}
+
+int Stack::pop() {
+    if (!isEmpty()) {
+        return data[top--];
+    }
+    std::cerr << "Stack underflow!" << std::endl;
+    return -1;
+}
+
+int Stack::peek() const {
+    if (!isEmpty()) {
+        return data[top];
+    }
+    std::cerr << "Stack is empty!" << std::endl;
+    return -1;
+}
+
+bool Stack::isEmpty() const {
+    return top == -1;
+}

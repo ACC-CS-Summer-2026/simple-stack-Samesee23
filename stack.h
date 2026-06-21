21f8a8ff977ef324f309b8c535c8b21223d91e8d
cosc2436 <mafloresrr@gmail.com>
2026-06-21 06:49:23 +0000

    added header to file

--- stack.h
+++ stack.h
@@ -1,3 +1,9 @@
+/* *************************************************
+*  Name: Michael Flores
+*  Assignment: 4 - Stack Class Implementation
+*  Purpose: Create a Stack class with push, pop, peek, and isEmpty methods
+************************************************* */
+
 #ifndef STACK_H
 #define STACK_H
 
@@ -6,14 +12,23 @@ const int DEFAULT_SIZE = 10;
 
 class Stack {
 private:
+    /********************************************
+    Private member variables for the Stack class.
+    *********************************************/
     int* data;
     int top;
     int capacity;
 
 public:
+    /**********************
+    Constructors/Destructor
+    ***********************/
     Stack();
     ~Stack();
     
+    /**********************
+    Public member functions for the Stack class.
+    ***********************/
     void push(int value);
     int pop();
     int peek() const;
